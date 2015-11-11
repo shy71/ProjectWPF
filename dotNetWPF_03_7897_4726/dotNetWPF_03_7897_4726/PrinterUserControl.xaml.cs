cd81f9a7bb74212dab4a67bd0176e2eeb88fcc2b
Shy Tennenbaum <shytennenbaum@gmail.com>
2015-11-11 18:02:34 +0200

    Added Basic Intengers to PrinterUserControl

--- dotNetWPF_03_7897_4726/dotNetWPF_03_7897_4726/PrinterUserControl.xaml.cs
+++ dotNetWPF_03_7897_4726/dotNetWPF_03_7897_4726/PrinterUserControl.xaml.cs
@@ -20,6 +20,43 @@ namespace dotNetWPF_03_7897_4726
     /// </summary>
     public partial class PrinterUserControl : UserControl
     {
+        int MAX_INK = 100, MAX_PAGES = 400;
+        string printerName;
+        public string PrinterName
+        {
+            get { return printerName; }
+            set
+            {
+                printerName = value;
+                printerNameLabel.Content = printerName;
+            }
+        }
+        double inkCount;
+        public double InkCount
+        {
+            get { return inkCount; }
+            set
+            {
+                if (value > MAX_INK)
+                    inkCount = MAX_INK;
+                else
+                    inkCount = System.Math.Round(value, 1);
+                inkLabel.Content = inkCount;
+            }
+        }
+        int pageCount;
+        public int PageCount
+        {
+            get { return pageCount; }
+            set
+            {
+                if (value > MAX_PAGES)
+                    pageCount = MAX_PAGES;
+                else
+                    PageCount = value;
+                pageLabel.Content = pageCount;
+            }
+        }
         EventHandler<PrinterEventArgs> PageMissing, InkEmpty;
         public PrinterUserControl()
         {
