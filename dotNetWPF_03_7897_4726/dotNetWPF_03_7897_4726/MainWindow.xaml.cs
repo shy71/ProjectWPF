61d96a0810ca097b34ba892745babcf421c1f885
Shy Tennenbaum <shytennenbaum@gmail.com>
2015-11-20 01:15:21 +0200

    Added TODO comment

--- dotNetWPF_03_7897_4726/dotNetWPF_03_7897_4726/MainWindow.xaml.cs
+++ dotNetWPF_03_7897_4726/dotNetWPF_03_7897_4726/MainWindow.xaml.cs
@@ -20,6 +20,12 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+/*
+ * TODO: need to delete all not necessary comments
+ * TODO: check the threding
+ */
+
+
 namespace dotNetWPF_03_7897_4726
 {
     /// <summary>
