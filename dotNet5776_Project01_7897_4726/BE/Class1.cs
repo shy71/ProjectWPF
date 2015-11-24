a21bd802f14ce73bad49d1e72ddc83dae94aa21d
ee bb <ezra@anyblock.com>
2015-11-24 21:40:31 +0200

    added project 1 solution and started BE library and the class order

---
+++ dotNet5776_Project01_7897_4726/BE/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Class1
+    {
+        enum kashrut
+        {
+            low,medium,high
+        }
+        /// <summary>
+        /// הזמנה
+        /// </summary>
+        public class Order
+        {
+            int orderNum;
+            DateTime date;
+            int branchNum;
+            kashrut orderKashrut;
+            string customerName;
+            string address;
+        }
+    }
+}
