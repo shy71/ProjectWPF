a344564f0c15dc93cf28de165b9c7f2de2c3a707
Shy Tennenbaum <shytennenbaum@gmail.com>
2015-11-25 17:58:03 +0200

    Added DataBase

---
+++ dotNet5776_Project01_7897_4726/DS/DataSource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS
+{
+    public class DataSource
+    {
+        static public List<BE.Dish> DishList;
+        static public List<BE.Branch> BranchList;
+        static public List<BE.Client> ClientList;
+        static public List<BE.DishOrder> DishOrderList;
+        static public List<BE.Order> OrderList;
+    }
+}
