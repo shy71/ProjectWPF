4ae5707c4f02b2b5836487e792ae053b16d31b05
shy <shytennenabum@gmail.com>
2015-12-03 15:21:41 +0200

    added new for lists

--- dotNet5776_Project01_7897_4726/DS/DataSource.cs
+++ dotNet5776_Project01_7897_4726/DS/DataSource.cs
@@ -8,15 +8,15 @@ namespace DS
 {
     public class DataSource
     {
-        static public List<BE.Dish> DishList;
+        static public List<BE.Dish> DishList = new List<BE.Dish>();
         static public int DishIDCounter;
-        static public List<BE.Branch> BranchList;
+        static public List<BE.Branch> BranchList = new List<BE.Branch>();
         static public int BranchIDCounter;
-        static public List<BE.Client> ClientList;
+        static public List<BE.Client> ClientList = new List<BE.Client>();
         static public int ClientIDCounter;
-        static public List<BE.DishOrder> DishOrderList;
+        static public List<BE.DishOrder> DishOrderList = new List<BE.DishOrder>();
         static public int DishOrderIDCounter;
-        static public List<BE.Order> OrderList;
+        static public List<BE.Order> OrderList = new List<BE.Order>();
         static public int OrderIDCounter;
     }
 }
