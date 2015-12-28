56c0f3f374e50c6698be66e310701b2ef9ab1dd9
shy <shytennenbaum@gmail.com>
2015-12-28 14:20:27 +0200

    Delete counters

--- dotNet5776_Project01_7897_4726/DS/DataSource.cs
+++ dotNet5776_Project01_7897_4726/DS/DataSource.cs
@@ -9,14 +9,9 @@ namespace DS
     public class DataSource
     {
         static public List<BE.Dish> DishList = new List<BE.Dish>();
-        static public int DishIDCounter;
         static public List<BE.Branch> BranchList = new List<BE.Branch>();
-        static public int BranchIDCounter;
         static public List<BE.Client> ClientList = new List<BE.Client>();
-        static public int ClientIDCounter;
         static public List<BE.DishOrder> DishOrderList = new List<BE.DishOrder>();
-        static public int DishOrderIDCounter;
         static public List<BE.Order> OrderList = new List<BE.Order>();
-        static public int OrderIDCounter;
     }
 }
