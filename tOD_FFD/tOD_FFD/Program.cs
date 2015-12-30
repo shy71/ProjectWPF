90b5923734fa83f469935dc1cca048a50edaacb4
Shy Tennenbaum <shytennenbaum@gmail.com>
2015-12-30 13:41:35 +0200

    Sync

--- tOD_FFD/tOD_FFD/Program.cs
+++ tOD_FFD/tOD_FFD/Program.cs
@@ -3,19 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace tOD_FFD
 {
-    class Program
-    {/*
-         public interface IKey {     int Key { get; } }  
-        public class Name {     public string FirstName { get; set; }     public string LastName { get; set; } } 
-        public enum Gender { male, female } public class Person : IKey{     public int Id { get; set; }     public Name PersonName { get; set; }     public Gender PersonGender { get; set; }     public bool Married { get; set; }      public int Key     {         get         {             return Id;         }     } }
-        public enum Gender { male, female }
-        public class Person : IKey { public int Id { get; set; } public Name PersonName { get; set; } public Gender PersonGender { get; set; } public bool Married { get; set; } public int Key { get { return Id; } } }  
-        public class MyList<T> where T : IKey { List<T> list; public MyList() { T temp; list = new List<T>(); } public bool add(T t) { if (list.Exists(v => v.Key == t.Key))             return false; list.Add(t); return true; } public bool remove(T t) { T temp = list.Find(v => v.Key == t.Key); return list.Remove(temp); } }  
+    class temp
+    {
+        public static int MakeID()
+        {
+            string Name="shy", Size="large", Price="78";
+            string[] DishComponents = new string[3]{Name,Size,Price};
+            //DishComponents[0] = Name;
+            //DishComponents[1] = Size;
+            //DishComponents[2] = Price;
+            return DishComponents.Sum<Func<string,object>>((item) => (item.Select(r => (int)r).ToArray()));
+        }
+    }
 
-        static void Main(string[] args) { Person p = new Person { Id = 123, Married = true, PersonGender = Gender.male, PersonName = new Name { FirstName = "oshri", LastName = " Cohen" } }; MyList<Person> personList = new MyList<Person>(); Console.WriteLine(personList.add(p)); Person p2 = new Person { Id = 123, Married = false, PersonGender = Gender.female, PersonName = new Name { FirstName = "oshri", LastName = " Cohen" } }; Console.WriteLine(personList.add(p2)); Person p3 = new Person { Id = 1234, Married = false, PersonGender = Gender.female, PersonName = new Name { FirstName = "oshri", LastName = " Cohen" } }; Console.WriteLine(personList.add(p3)); 
-        }  */
+    class Program
+    {
+        static public void Main()
+        {
+            temp.MakeID();
+        }
+
+
     }
 }
