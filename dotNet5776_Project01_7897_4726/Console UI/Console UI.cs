19f463e8ce8c2c6e84738606bbf03810832c6c22
shy <shytennenbaum@gmail.com>
2016-01-05 17:49:43 +0200

    Sync

--- dotNet5776_Project01_7897_4726/Console UI/Console UI.cs
+++ dotNet5776_Project01_7897_4726/Console UI/Console UI.cs
@@ -42,26 +42,11 @@ namespace Console_UI
         public void Run()
         {
             myBL.Inti();
-            //myBL.PrintAll();
-            //myBL.AddBranch(new Branch("beer sheva", "rechov a 1/2", "0585205020", "bob", 4, 0, Kashrut.MEDIUM));
-            //myBL.AddClient(new Client("yair", "jerusalem 3/4", 7812873, 1899));
-            ////myBL.DeleteBranch(new Branch("Eilat", "freedom 98", "078496352", "oshri", 5, 3, Kashrut.LOW, 2));
-            //myBL.DeleteOrder(192334);
-
-            //Console.WriteLine("************************************************\n\n\n");
-            //Console.WriteLine(myBL.PriceOfOrder(new Order(2, "Beit Shemesh", DateTime.Now, Kashrut.LOW, 10934, 192334)));
-            //Console.WriteLine("************************************************\n\n\n");
-            //myBL.PrintAll();
-
-            ////finish UI and check all of the functions
-            int temp;
-            bool check;
             bool exit = false;
             while (!exit)
             {
                 try
                 {
-                    string str;
                     switch (Menu())
                     {
                         case 1:
