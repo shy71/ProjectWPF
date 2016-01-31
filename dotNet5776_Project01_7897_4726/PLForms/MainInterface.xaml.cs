c7e091ee322a09f427b064fa1688bb9e8f803fd9
Shy Tennenbaum <shytennenbaum@gmail.com>
2016-01-31 18:12:28 +0000

    Updated MainInterface.xaml.cs

--- dotNet5776_Project01_7897_4726/PLForms/MainInterface.xaml.cs
+++ dotNet5776_Project01_7897_4726/PLForms/MainInterface.xaml.cs
@@ -145,7 +145,6 @@ namespace PLForms
             //new ShowUserControl(new DishOrder(BL.FactoryBL.getBL().GetAllDishOrders().ElementAt(2))).Show();
             //new OrderEditor1(BL.FactoryBL.getBL().GetAllClients().First()).Show();
             //new DishPicker(BE.Kashrut.LOW, BL.FactoryBL.getBL().GetAllOrders().First().ID).Show();
-            new ClientInterface(BL.FactoryBL.getBL().GetAllUsers(item=>item.Type==BE.UserType.Client).First()).Show();
             this.Close();
         }
 
