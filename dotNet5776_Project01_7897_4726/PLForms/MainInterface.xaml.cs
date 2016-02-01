a032727203410d0d15b929df0ad3c7801b36d4e2
ee bb <ezra@anyblock.com>
2016-02-01 18:08:05 +0000

    Updated MainInterface.xaml.cs

--- dotNet5776_Project01_7897_4726/PLForms/MainInterface.xaml.cs
+++ dotNet5776_Project01_7897_4726/PLForms/MainInterface.xaml.cs
@@ -140,12 +140,12 @@ namespace PLForms
 
         private void Button_Click(object sender, RoutedEventArgs e)//delete
         {
-            //new Profit_By().Show();
+            new Profit_Details().ShowDialog();
             //new ShowUserControl(new DishOrder(BL.FactoryBL.getBL().GetAllDishOrders().First())).Show();
             //new ShowUserControl(new DishOrder(BL.FactoryBL.getBL().GetAllDishOrders().ElementAt(2))).Show();
             //new OrderEditor1(BL.FactoryBL.getBL().GetAllClients().First()).Show();
             //new DishPicker(BE.Kashrut.LOW, BL.FactoryBL.getBL().GetAllOrders().First().ID).Show();
-            new DishEditor(BL.FactoryBL.getBL().GetAllDishs().First()).Show();
+            //new DishEditor(BL.FactoryBL.getBL().GetAllDishs().First()).Show();
             this.Close();
         }
 
